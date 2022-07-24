6a8ef2a3390548bb3a3812f565f5eb5d6a9519f8
Benjamín Guzmán <bg@benjaminguzman.dev>
2022-07-24 15:45:07 -0500

    Updated script

--- README.md
+++ README.md
@@ -8,8 +8,11 @@ Run basic stuff, like update the system, disable some services like sshd, instal
 You can also select to install programming tools for different languages like Go, C, Java and NodeJS.
 
 The script is really simple, actually it could be entirely written in bash, but I wanted to practice my Go skills.
+
 Because the source code is simple, you can easily modify it and adjust it to your needs.
 
 ## Other configurations
 
-This repo also includes configuration files for [terminator](https://gnome-terminator.readthedocs.io/en/latest/) and [latte dock](https://github.com/KDE/latte-dock) configuration. It is also worth to check this other [vim](https://github.com/amix/vimrc) configuration
+This repo also includes configuration files for [terminator](https://gnome-terminator.readthedocs.io/en/latest/) and 
+[latte dock](https://github.com/KDE/latte-dock).
+It is also worth to check this other [vim](https://github.com/amix/vimrc) configuration
--- new-install.go
+++ new-install.go
@@ -1,57 +1,60 @@
 package main
 
 import (
-	"fmt"
 	"flag"
-    "github.com/BenjaminGuzman/fedora-new-install/cmd"
+	"fmt"
+	"github.com/BenjaminGuzman/fedora-new-install/cmd"
 )
 
 type InstallOptions struct {
-	node bool // indicates if NodeJS should be installed
-	java bool // indicates if Java should be installed
-	go_ bool // indicates if Go should be installed
-	math bool // indicates if math software should be installed
-	c bool // indicates if C Development Tools and Libraries should be installed
+	node      bool // indicates if NodeJS should be installed
+	java      bool // indicates if Java should be installed
+	go_       bool // indicates if Go should be installed
+	math      bool // indicates if math software should be installed
+	c         bool // indicates if C Development Tools and Libraries should be installed
+	librewolf bool
+	ohMyZsh   bool
 }
 
 func getOptions() *InstallOptions {
 	// define flags
 	var opts InstallOptions
 	var installAllLangs bool
-	flag.BoolVar(&opts.node, "node", false, "Indicate if NodeJS should be installed. Default: false")
-	flag.BoolVar(&opts.java, "java", false, "Indicate if Java 11 (devel) should be installed. Default: false")
-	flag.BoolVar(&opts.go_, "go", false, "Indicate if Go should be installed. Default: false")
-	flag.BoolVar(&opts.c, "c", false, "Indicate if dev tools and libraries for C should be installed. Default: false")
-	flag.BoolVar(&opts.math, "math", false, "Indicate if math tools (octave, rstudio, texstudio) should be installed. Default: false")
+	flag.BoolVar(&opts.node, "node", false, "Indicate if NodeJS should be installed")
+	flag.BoolVar(&opts.java, "java", false, "Indicate if Java 11 & 17 (devel) should be installed")
+	flag.BoolVar(&opts.go_, "go", false, "Indicate if Go should be installed")
+	flag.BoolVar(&opts.c, "c", false, "Indicate if dev tools and libraries for C should be installed")
+	flag.BoolVar(&opts.math, "math", false, "Indicate if math tools (octave, rstudio, texstudio) should be installed")
 	flag.BoolVar(
-		&installAllLangs, 
-		"langs", 
-		false, 
-		"Indicate if you want to install all languages (NodeJS, Java, Go, C). Default: false. Overrides -node, -java, -go, -c",
+		&installAllLangs,
+		"langs",
+		false,
+		"Indicate if you want to install all languages (NodeJS, Java, Go, C). Default: false. Overrides --node, --java, --go, --c",
 	)
-	
+	flag.BoolVar(&opts.librewolf, "librewolf", false, "Indicate if LibreWolf should be installed")
+
 	// parse CLI flags
 	flag.Parse()
 
 	if installAllLangs {
 		opts.node, opts.java, opts.go_, opts.c = true, true, true, true
 	}
-	
-	return &opts;
+
+	return &opts
 }
 
 func main() {
 	var opts *InstallOptions = getOptions()
 	var pendingCmds []*cmd.Cmd = []*cmd.Cmd{
-		cmd.NewCmd("Update system", "dnf update -y"),
+		cmd.NewCmd("Update system", "dnf update -y --refresh"),
 		cmd.NewCmd(
-			"Install useful programs", 
-			"dnf install -y latte-dock terminator thunderbird vim firewalld firewall-config keepassxc chromium kgpg stacer dia git",
+			"Install useful programs",
+			"dnf install -y latte-dock terminator thunderbird vim firewalld firewall-config chromium kgpg stacer dia git htop peek mpv",
 		),
 		cmd.NewCmd("Disable services", "systemctl disable --now cups sshd geoclue"),
-        cmd.NewCmd("Set firewall default zone to block", "firewall-cmd --set-default-zone=block"),
+		cmd.NewCmd("Set firewall default zone to block", "firewall-cmd --set-default-zone=block"),
 		cmd.NewCmd("Remove package managers", "dnf install -y flatpak snapd"),
-		cmd.NewCmd("Install sec & net tools", "dnf install -y nmap wireshark clamav clamav-update rkhunter"),
+		cmd.NewCmd("Install security & network tools", "dnf install -y nmap wireshark clamav clamav-update rkhunter"),
 		cmd.NewCmd("Lock root account", "usermod -L root"),
 	}
 
@@ -59,32 +62,64 @@ func main() {
 		pendingCmds = append(pendingCmds, cmd.NewCmd("Install Node JS", "dnf install -y nodejs"))
 	}
 	if opts.java {
-		pendingCmds = append(pendingCmds, cmd.NewCmd(
-			"Install Java",
-			"dnf install -y java-11-openjdk-src java-11-openjdk-javadoc java-11-openjdk-devel maven",
-		))
+		pendingCmds = append(
+			pendingCmds,
+			cmd.NewCmd("Install maven", "dnf install -y maven"),
+			cmd.NewCmd(
+				"Install Java 11",
+				"dnf install -y java-11-openjdk-src java-11-openjdk-javadoc java-11-openjdk-devel",
+			),
+			cmd.NewCmd(
+				"Install Java 17",
+				"dnf install -y java-17-openjdk-src java-17-openjdk-javadoc java-17-openjdk-devel",
+			),
+		)
 	}
 	if opts.go_ {
 		pendingCmds = append(pendingCmds, cmd.NewCmd("Install Go", "dnf install -y golang"))
 	}
 	if opts.math {
-		pendingCmds = append(pendingCmds, cmd.NewCmd("Install Math utilites", "dnf install -y octave rstudio texstudio"))
+		pendingCmds = append(pendingCmds, cmd.NewCmd("Install Math utilities", "dnf install -y octave rstudio texstudio"))
 	}
 	if opts.c {
 		pendingCmds = append(pendingCmds, cmd.NewCmd("Install C Dev tools", "dnf group install -y \"C Development Tools and Libraries\""))
+	}
+	if opts.librewolf {
+		pendingCmds = append(
+			pendingCmds,
+			cmd.NewCmd(
+				"Import LibeWolf GPG key",
+				"rpm --import https://keys.openpgp.org/vks/v1/by-fingerprint/034F7776EF5E0C613D2F7934D29FBD5F93C0CFC3",
+			),
+			cmd.NewCmd(
+				"Add LibreWold repo",
+				"dnf config-manager --add-repo https://rpm.librewolf.net",
+			),
+			cmd.NewCmd(
+				"Install LibreWolf",
+				"sudo dnf install -y librewolf",
+			),
+		)
+	}
+	if opts.ohMyZsh {
+		pendingCmds = append(
+			pendingCmds,
+			cmd.NewCmd("Install zsh", "dnf install -y zsh"),
+			cmd.NewCmd("Install oh-my-zsh", "echo Installation of oh my zsh is interactive, so you'd need to run sh -c \"$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)\""),
+		)
 	}
 
 	for _, c := range pendingCmds {
 		c.Run()
 	}
 
-    fmt.Println("\n------------------------------")
-    fmt.Println("All commands were run")
+	fmt.Println("\n------------------------------")
+	fmt.Println("All commands were run")
 	fmt.Println("-- Final recommendations --")
-	fmt.Println("1. Install librewolf https://librewolf.net Don't use firefox")
+	fmt.Println("1. Don't use firefox. Use librewolf instead")
 	fmt.Println("2. Set static IP")
 	fmt.Println("3. Configure the DNS to block adds. See https://github.com/Ultimate-Hosts-Blacklist/Ultimate.Hosts.Blacklist")
-    fmt.Println("4. Disable file search")
+	fmt.Println("4. Disable file search")
 
 	fmt.Println("\nHave a nice experience with your new Fedora installation 🙃")
 }
